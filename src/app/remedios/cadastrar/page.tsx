0c71881c48335bf943d62428bc45e3e0469acb27
Rodolpho Alves <rrpa15@live.com>
2024-03-20 00:31:41 -0300

    adicionando validação

--- src/app/remedios/cadastrar/page.tsx
+++ src/app/remedios/cadastrar/page.tsx
@@ -1,17 +1,49 @@
 "use client"
 
 import { RemedioCadastro } from "@/app/view/remedy"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import styles from "./cadastrar.module.css";
 import { useRouter } from "next/navigation";
+
+interface ValidadorRemedio {
+    nome: {
+        ativo: boolean,
+        mensagem: string
+    },
+    via: {
+        ativo: boolean,
+        mensagem: string
+    },
+    validade: {
+        ativo: boolean,
+        mensagem: string
+    },
+    laboratorio: {
+        ativo: boolean,
+        mensagem: string
+    },
+    lote: {
+        ativo: boolean,
+        mensagem: string
+    }
+}
+
+const NOME_EM_BRANCO_MSG = "O nome não pode estar em branco."
+const NOME_LONGO_MSG = "O nome deve ter no máximo 20 caracteres."
+const VIA_DESSELECIONADA_MSG = "A via deve ser selecionada!"
+const LOTE_EM_BRANCO_MSG = "O lote não pode estar em branco."
+const VALIDADE_NO_PASSADO_MSG = "A data de validade deve ser no futuro."
+const VALIDADE_DESSELECIONADA_MSG = "A validade deve ser selecionada!"
+const LAB_DESSELECIONADO_MSG = "A laboratório deve ser selecionado!"
 
 const fetcher = (
     url: string, options?: RequestInit
 ) => fetch(url, options).then((res) => res.json())
 
 export default function CadastrarRemedio() {
-
+    
+    const dataAtual = new Date().toISOString().split('T')[0]
     const router = useRouter()
 
     const [createDTO, setCreateDTO] = useState<RemedioCadastro>({
@@ -23,41 +55,73 @@ export default function CadastrarRemedio() {
         via: ""
     })
 
-    const vias : string[] = ["ORAL", "NASAL", "VENOSO", "INTRAMUSCULAR", "RETAL"]
-    const laboratorios : string[] = ["MEDLEY", "LABEXEMP"]
+    const [formularioPronto, setFormularioPronto] = useState(true)
 
-    const handleChangeName = (name : string) => {
+    const [validador, setValidador] = useState<ValidadorRemedio>({
+        nome: {
+            ativo: false,
+            mensagem: ""
+        },
+        via: {
+            ativo: false,
+            mensagem: ""
+        },
+        validade: {
+            ativo: false,
+            mensagem: ""
+        },
+        laboratorio: {
+            ativo: false,
+            mensagem: ""
+        },
+        lote: {
+            ativo: false,
+            mensagem: ""
+        }
+    })
+
+    useEffect(() => {
+        checarCampos()
+        setFormularioPronto(
+            Object.values(validador).every(val => val.ativo === false)
+        )
+    }, [createDTO])
+
+    const vias: string[] = ["ORAL", "NASAL", "VENOSO", "INTRAMUSCULAR", "RETAL"]
+    const laboratorios: string[] = ["MEDLEY", "LABEXEMP"]
+
+    const handleChangeName = (name: string) => {
         setCreateDTO({
-            ...createDTO, 
+            ...createDTO,
             nome: name
         })
     }
 
-    const handleChangeLote = (lote : string) => {
+    const handleChangeLote = (lote: string) => {
         setCreateDTO({
-            ...createDTO, 
+            ...createDTO,
             lote: lote
         })
     }
-    
-    const handleChangeValidade = (validade : string) => {
+
+    const handleChangeValidade = (validade: string) => {
         setCreateDTO({
-            ...createDTO, 
-            validade : validade
+            ...createDTO,
+            validade: validade
         })
     }
 
-    const handleChangeQuantidade = (quantidade : number) => {
-        if(quantidade < 0)
+    const handleChangeQuantidade = (quantidade: number) => {
+        if (quantidade < 0)
             return;
         setCreateDTO({
-            ...createDTO, 
-            quantidade : quantidade
+            ...createDTO,
+            quantidade: quantidade
         })
     }
 
-    const handleChangeVia = (via : string) => {
-        if(!vias.includes(via))
+    const handleChangeVia = (via: string) => {
+        if (!vias.includes(via))
             return;
         setCreateDTO({
             ...createDTO,
@@ -65,8 +129,8 @@ export default function CadastrarRemedio() {
         })
     }
 
-    const handleChangeLaboratorio = (lab : string) => {
-        if(!laboratorios.includes(lab))
+    const handleChangeLaboratorio = (lab: string) => {
+        if (!laboratorios.includes(lab))
             return;
         setCreateDTO({
             ...createDTO,
@@ -74,8 +138,51 @@ export default function CadastrarRemedio() {
         })
     }
 
-    const save = async () => {
+    function checarCampos() {
+        const agora = new Date();
+        const validade = new Date(createDTO.validade);
 
+        const validadorChecado = {
+            nome: {
+                ativo: !createDTO.nome || createDTO.nome.length > 20,
+                mensagem: !createDTO.nome
+                    ? NOME_EM_BRANCO_MSG
+                    : createDTO.nome.length > 20
+                        ? NOME_LONGO_MSG
+                        : ""
+            },
+            laboratorio: {
+                ativo: !createDTO.laboratorio,
+                mensagem: !createDTO.laboratorio
+                    ? LAB_DESSELECIONADO_MSG
+                    : ""
+            },
+            via: {
+                ativo: !createDTO.via,
+                mensagem: !createDTO.via
+                    ? VIA_DESSELECIONADA_MSG
+                    : ""
+            },
+            lote: {
+                ativo: !createDTO.lote,
+                mensagem: !createDTO.lote
+                    ? LOTE_EM_BRANCO_MSG
+                    : ""
+            },
+            validade: {
+                ativo: !createDTO.validade || validade.getTime() <= agora.getTime(),
+                mensagem: !createDTO.validade
+                    ? VALIDADE_DESSELECIONADA_MSG
+                    : validade.getTime() <= agora.getTime()
+                        ? VALIDADE_NO_PASSADO_MSG
+                        : ""
+            }
+        }
+
+        setValidador(validadorChecado)
+    }
+
+    const save = async () => {
         await fetcher('/api/remedios', {
             method: "POST",
             body: JSON.stringify(createDTO)
@@ -86,24 +193,253 @@ export default function CadastrarRemedio() {
 
 
     return (
-        <div className={styles.cadastrarRemedioContainer}>
+        <div className="flex flex-col w-full justify-center items-center">
+            <h2 className="
+                text-xl
+                uppercase
+                titulobg
+                font-black
+                mb-4">
+                Cadastrar remedio
+            </h2>
 
-            <h2>Cadastrar remedio</h2>
+            <div className="w-96 mb-4">
+                <label className="
+                block 
+                mb-2 
+                text-sm 
+                font-medium 
+                text-black">Nome</label>
 
-            <div className={`${styles.inputContainer} ${styles.nomeContainer}`}>
-                <label htmlFor="remedio-name">Nome</label>
+                <input
+                    autoComplete="off"
+                    type="text"
+                    name="remedio-name"
+                    value={createDTO.nome}
+                    onChange={(e) => { handleChangeName(e.target.value) }}
+                    className="
+                    bg-gray-50 border 
+                    border-gray-500 
+                    text-gray-900 
+                    placeholder-gray-700 
+                    text-sm 
+                    rounded-lg
+                    block 
+                    w-full 
+                    p-2.5"/>
+                <p className={`
+                    mt-2 
+                    text-sm 
+                    text-red-600
+                    ${validador.nome.ativo ? '' : 'invisible'}`}>
+                    <span className="font-medium">Atenção!</span> {validador.nome.mensagem}
+                </p>
+            </div>
+
+            <div className="w-96 mb-4">
+                <label className="
+                    block 
+                    mb-2 
+                    text-sm 
+                    font-medium 
+                    text-black">Via</label>
+
+                <select
+                    name="remedio-via"
+                    value={createDTO.via}
+                    onChange={(e) => { handleChangeVia(e.target.value) }}
+                    className="
+                        bg-gray-50 border 
+                        border-gray-500 
+                        text-gray-900 
+                        placeholder-gray-700 
+                        text-sm 
+                        rounded-lg
+                        block 
+                        w-full 
+                        p-2.5">
+                    <option value="">Selecione...</option>
+                    {vias.map((via) => (
+                        <option key={via} value={via}>{via}</option>
+                    ))}
+                </select>
+
+                <p className={`
+                    mt-2 
+                    text-sm 
+                    text-red-600
+                    ${validador.via.ativo ? '' : 'invisible'}`}>
+                    <span className="font-medium">Atenção!</span> {validador.via.mensagem}
+                </p>
+            </div>
+
+            <div className="w-96 mb-4">
+                <label className="
+                    block 
+                    mb-2 
+                    text-sm 
+                    font-medium 
+                    text-black">Laboratório</label>
+
+                <select
+                    name="remedio-laboratorio"
+                    value={createDTO.laboratorio}
+                    onChange={(e) => { handleChangeLaboratorio(e.target.value) }}
+                    className="
+                        bg-gray-50 border 
+                        border-gray-500 
+                        text-gray-900 
+                        placeholder-gray-700 
+                        text-sm 
+                        rounded-lg
+                        block 
+                        w-full 
+                        p-2.5">
+                    <option value="">Selecione...</option>
+                    {laboratorios.map((lab) => (
+                        <option key={lab} value={lab}>{lab}</option>
+                    ))}
+                </select>
+
+                <p className={`
+                    mt-2 
+                    text-sm 
+                    text-red-600
+                    ${validador.laboratorio.ativo ? '' : 'invisible'}`}>
+                    <span className="font-medium">Atenção!</span> {validador.laboratorio.mensagem}
+                </p>
+            </div>
+
+
+            <div className="w-96 mb-4">
+                <label className="
+                block 
+                mb-2 
+                text-sm 
+                font-medium 
+                text-black">Lote</label>
+
+                <input
+                    autoComplete="off"
+                    type="text"
+                    name="remedio-lote"
+                    value={createDTO.lote}
+                    onChange={(e) => { handleChangeLote(e.target.value) }}
+                    className="
+                    bg-gray-50 border 
+                    border-gray-500 
+                    text-gray-900 
+                    placeholder-gray-700 
+                    text-sm 
+                    rounded-lg
+                    block 
+                    w-full 
+                    p-2.5"/>
+                <p className={`
+                    mt-2 
+                    text-sm 
+                    text-red-600
+                    ${validador.lote.ativo ? '' : 'invisible'}`}>
+                    <span className="font-medium">Atenção!</span> {validador.lote.mensagem}
+                </p>
+            </div>
+
+            <div className="w-96 mb-8">
+                <label className="
+                block 
+                mb-2 
+                text-sm 
+                font-medium 
+                text-black">Quantidade</label>
+
+                <input
+                    autoComplete="off"
+                    type="number"
+                    name="remedio-quantidade"
+                    value={createDTO.quantidade}
+                    onChange={(e) => { handleChangeQuantidade(parseInt(e.target.value)) }}
+                    className="
+                    bg-gray-50 border 
+                    border-gray-500 
+                    text-gray-900 
+                    placeholder-gray-700 
+                    text-sm 
+                    rounded-lg
+                    block 
+                    w-full 
+                    p-2.5"/>
+            </div>
+
+            <div className="w-96 mb-4">
+                <label htmlFor="validade"   
+                    className="
+                    block 
+                    mb-2 
+                    text-sm 
+                    font-medium 
+                    text-black">Validade</label>
+
                 <input 
-                type="text" 
-                name="remedio-name" 
-                id=""
-                value={createDTO.nome}
-                onChange={(e) => {handleChangeName(e.target.value)}}
+                    type="date"
+                    id="validade"
+                    name="validade"
+                    min={dataAtual}
+                    value = {createDTO.validade}
+                    onChange={(e) => handleChangeValidade(e.target.value)}
+                    className="
+                        bg-gray-50 border 
+                        border-gray-500 
+                        text-gray-900 
+                        placeholder-gray-700 
+                        text-sm 
+                        rounded-lg
+                        block 
+                        w-full 
+                        p-2.5"/>
+
+                <p className={`
+                    mt-2 
+                    text-sm 
+                    text-red-600
+                    ${validador.validade.ativo ? '' : 'invisible'}`}>
+                    <span className="font-medium">Atenção!</span> {validador.validade.mensagem}
+                </p>
+            </div>
+
+            <button 
+            disabled={formularioPronto}
+            className={`
+                flex text-xl  
+                px-6 py-2 
+                rounded-md
+                bg-gradient-to-b from-cyan-100 to-white 
+                text-slate-900
+                hover:bg-gradient-to-b hover:from-cyan-200 hover:to-white 
+                ${formularioPronto? `
+                    disabled:bg-gradient-to-b 
+                    disabled:from-gray-200 
+                    disabled:to-white
+                    disabled:text-red-900
+                    disabled:cursor-not-allowed`: ''} 
+            `} 
+            onClick={save}>
+                Enviar
+            </button>
+
+            {/* <div className={`${styles.inputContainer} ${styles.nomeContainer}`}>
+                <label htmlFor="remedio-name">Nome</label>
+                <input
+                    type="text"
+                    name="remedio-name"
+                    id=""
+                    value={createDTO.nome}
+                    onChange={(e) => { handleChangeName(e.target.value) }}
                 />
             </div>
 
             <div className={`${styles.inputContainer} ${styles.viaContainer}`}>
                 <label htmlFor="via">Via</label>
-                <select name="via" id="" onChange={(e) => {handleChangeVia(e.target.value)}}>
+                <select name="via" id="" onChange={(e) => { handleChangeVia(e.target.value) }}>
                     <option value={""}>Selecione ...</option>
                     {
                         vias.map((option, index) => (
@@ -115,7 +451,7 @@ export default function CadastrarRemedio() {
 
             <div className={`${styles.inputContainer} ${styles.labortorioContainer}`}>
                 <label htmlFor="laboratorio">Laboratório</label>
-                <select name="laboratorio" id="" onChange={(e) => {handleChangeLaboratorio(e.target.value)}}>
+                <select name="laboratorio" id="" onChange={(e) => { handleChangeLaboratorio(e.target.value) }}>
                     <option value={""}>Selecione ...</option>
                     {
                         laboratorios.map((option, index) => (
@@ -127,38 +463,40 @@ export default function CadastrarRemedio() {
 
             <div className={`${styles.inputContainer} ${styles.loteContainer}`}>
                 <label htmlFor="lote">Lote</label>
-                <input 
-                type="text" 
-                name="lote"
-                value={createDTO.lote}
-                onChange={(e) => handleChangeLote(e.target.value)}
+                <input
+                    type="text"
+                    name="lote"
+                    value={createDTO.lote}
+                    onChange={(e) => handleChangeLote(e.target.value)}
                 />
             </div>
 
             <div className={`${styles.inputContainer} ${styles.quantidadeContainer}`}>
                 <label htmlFor="quantidade">Quantidade</label>
-                <input 
-                type="number" 
-                name="quantidade"
-                value={createDTO.quantidade}
-                onChange={(e) => handleChangeQuantidade(parseInt(e.target.value))}
+                <input
+                    type="number"
+                    name="quantidade"
+                    value={createDTO.quantidade}
+                    onChange={(e) => handleChangeQuantidade(parseInt(e.target.value))}
                 />
             </div>
 
 
             <div className={`${styles.inputContainer} ${styles.validadeContainer}`}>
                 <label htmlFor="validade">Validade</label>
-                <input 
-                type="date" 
-                name="validade"
-                value={createDTO.validade}
-                onChange={(e) => handleChangeValidade(e.target.value)}
+                <input
+                    type="date"
+                    name="validade"
+                    value={createDTO.validade}
+                    onChange={(e) => handleChangeValidade(e.target.value)}
                 />
-            </div>
+            </div> */}
 
-            <div className={`${styles.submitButton}`} onClick={save}>
+            {/* <div className={`${styles.submitButton}`} onClick={save}>
                 Enviar
-            </div>
+            </div> */}
+
+           
 
         </div>
     )
